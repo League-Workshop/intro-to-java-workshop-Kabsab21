e1addafec683c75c99b2040b97f5e98c39e50878
league <league@imac-18.attlocal.net>
2021-11-12 17:00:27 -0800

    pop ups still

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,14 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String owo = JOptionPane.showInputDialog(null,"cawn u write code owo");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(owo.equals("ye")) {
+	JOptionPane.showMessageDialog(null,"nerwd   owo");
 		// 3. Otherwise, wish them good luck washing dishes.
 
-	}
-}
+	}else {
+		JOptionPane.showMessageDialog(null,"ur a nerwd owo");
+		}
+}}
 
--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,35 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int owo = new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(owo);
 	// 4. Get the user to enter something that they think is awesome
-
+String uwu = JOptionPane.showInputDialog(null,"Wat do ya think is awesome owo");
 	// 5. If the random number is 0
 
 	// -- tell the user whatever they entered is awesome!
-
+if(owo==0) {JOptionPane.showMessageDialog(null,"u cool");
 	// 6. If the random number is 1
-
+if(owo==1) {JOptionPane.showMessageDialog(null,"u ok");
 	// -- tell the user whatever they entered is ok.
-
+if(owo==2) {JOptionPane.showMessageDialog(null,"u boring");
 	// 7. If the random number is 2
 
 	// -- tell the user whatever they entered is boring.
-
+if(owo==3) {JOptionPane.showMessageDialog(null,"u ugly");
 	// 8. If the random number is 3
 
 	// -- write your own answer
 
-}
+} } } } } }
 
 

d1e3cc972d3b0ced36232e72b24f761842410aae
Kabsab21 <91648784+Kabsab21@users.noreply.github.com>
2021-11-26 17:00:55 -0800

    Update RobotTreasureHunt.java

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,50 +16,54 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	 Robot free =new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		free.setAngle(4);
+		free.microMove(10);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		free.setAngle(0); 
+		free.microMove(-2);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+		free.setAngle(76); 
+		free.microMove(568);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		free.setAngle(7); free.microMove(6578);
 	}
-
+//
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = free.getX();
+		int robotYLocation = free.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+	System.out.println("Robot Y location is :" + robotYLocation);
+	System.out.println("Robot X location is :" +robotXLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
-		
+	JOptionPane.showMessageDialog(null,"naw go left");
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
+
 		// 9.  If the robot is in the final location
-		//     --call the treasureFound() method
+		call the treasureFound() method 
 		
-	}
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
 		Robot.setWindowImage("section4/treasure_hunt.jpg");
 	
+	}
 		JOptionPane.showMessageDialog(null, "Ask the girl for help with your quest. Press the space bar to ask.");
 
 	}
